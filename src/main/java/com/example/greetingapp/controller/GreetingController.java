cfbd5404de76e540f465093aa21580c165534dc1
Akhil Puri <ap4723@srmist.edu.in>
2026-03-11 22:47:08 +0530

    [Akhil] Added: UC2: Extend Greeting Controller to use Services Layer

--- src/main/java/com/example/greetingapp/controller/GreetingController.java
+++ src/main/java/com/example/greetingapp/controller/GreetingController.java
@@ -1,49 +1,22 @@
 package com.example.greetingapp.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.greetingapp.service.GreetingService;
+
 @RestController
-@RequestMapping("/greeting")
 public class GreetingController {
-    // Get Method
-    @GetMapping
-    public Map<String, String> getGreeting(){
-        Map<String, String> response = new HashMap<>();
-        response.put("message", "Hello from GET Method");
-        return response;
+
+    private final GreetingService greetingService;
+
+    // Constructor Injection is used to inject the GreetingService dependency but also can be done using @Autowired annotation on the field or setter method.
+    public GreetingController(GreetingService greetingService) {
+        this.greetingService = greetingService;
     }
 
-    // Post Method
-    @PostMapping
-    public Map<String, String> postGreeting(@RequestBody Map<String, String> body){
-        Map<String, String> response = new HashMap<>();
-        response.put("message", "Hello " + body.get("name") + " from POST Method");
-        return response;
-    }
-
-    // PUT Method
-    @PutMapping("/{name}")
-    public Map<String, String> putGreeting(@PathVariable String name) {
-        Map<String, String> response = new HashMap<>();
-        response.put("message", "Hello " + name + " from PUT method");
-        return response;
-    }
-
-    // DELETE Method
-    @DeleteMapping("/{name}")
-    public Map<String, String> deleteGreeting(@PathVariable String name) {
-        Map<String, String> response = new HashMap<>();
-        response.put("message", name + " deleted successfully");
-        return response;
+    @GetMapping("/greeting")
+    public String getGreeting() {
+        return greetingService.getGreetingMessage();
     }
 }
