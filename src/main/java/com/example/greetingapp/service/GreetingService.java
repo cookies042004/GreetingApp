354b800b5e12bb4fcbd1a61f84304a4954e74966
Akhil Puri <ap4723@srmist.edu.in>
2026-03-12 11:54:41 +0530

    [Akhil] Added: UC6: Ability for the Greeting App to List all the Greeting Messages in the Repository

--- src/main/java/com/example/greetingapp/service/GreetingService.java
+++ src/main/java/com/example/greetingapp/service/GreetingService.java
@@ -1,10 +1,12 @@
 package com.example.greetingapp.service;
 
+import java.util.List;
+
 import com.example.greetingapp.model.GreetingModel;
 import com.example.greetingapp.repository.GreetingRepository;
+
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
 
 @Service
 public class GreetingService {
@@ -15,13 +17,7 @@ public class GreetingService {
         this.greetingRepository = greetingRepository;
     }
 
-    public GreetingModel findGreetingById(Long id){
-        Optional<GreetingModel> greeting = greetingRepository.findById(id);
-        
-        if(greeting.isPresent()){
-            return greeting.get();
-        } else {
-            throw new RuntimeException("Greeting not found with id: " + id);
-        }
+    public List<GreetingModel> getAllGreetings() {
+        return greetingRepository.findAll();
     }
 }
