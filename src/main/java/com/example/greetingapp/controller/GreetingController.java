fc264f3e359ed8060c99a9caedf091088e888a82
Akhil Puri <ap4723@srmist.edu.in>
2026-03-12 10:37:55 +0530

    [Akhil] Added: UC3: Ability for the Greeting App to give Greeting message with and without name

--- src/main/java/com/example/greetingapp/controller/GreetingController.java
+++ src/main/java/com/example/greetingapp/controller/GreetingController.java
@@ -1,6 +1,7 @@
 package com.example.greetingapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.greetingapp.service.GreetingService;
@@ -16,7 +17,7 @@ public class GreetingController {
     }
 
     @GetMapping("/greeting")
-    public String getGreeting() {
-        return greetingService.getGreetingMessage();
+    public String getGreeting(@RequestParam(required = false) String firstName, @RequestParam(required = false) String lastName) {
+        return greetingService.getGreetingMessage(firstName, lastName);
     }
 }
--- src/main/java/com/example/greetingapp/service/GreetingService.java
+++ src/main/java/com/example/greetingapp/service/GreetingService.java
@@ -4,8 +4,20 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class GreetingService {
+    public String getGreetingMessage(String firstName, String lastName) {
 
-    public String getGreetingMessage() {
+        if (firstName != null && lastName != null) {
+            return "Hello " + firstName + " " + lastName;
+        }
+
+        if (firstName != null) {
+            return "Hello " + firstName;
+        }
+
+        if (lastName != null) {
+            return "Hello " + lastName;
+        }
+
         return "Hello World";
     }
 }
